ad8b39306abdafbbbdab8ea6f086968c44af7c69
Alexey Shamrin <shamrin@gmail.com>
2021-01-15 02:29:29 +0200

    Create placeholder.ts

---
+++ placeholder.ts
@@ -0,0 +1,16 @@
+interface Query {
+    query: string
+    params: any[]
+}
+
+function sql(parts: string[], ...params: any[]) {
+    console.log(parts)
+  let query = parts.map((part, index) => {
+    if (index < parts.length - 1) part += '$' + (index + 1)
+    return part
+  }).join('')
+  return {query, params}
+}
+
+sql`select name from user where userid = ${42}`)
+// => { "query": "select name from user where userid = $1", "params": [ 42 ] } 
